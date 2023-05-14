4d46062bcdcffd9df6fc4a0dcc17ee46f024b475
NoorAhmed786 <aniiahmed380@gmail.com>
2023-05-15 04:13:03 +0500

    legal

---
+++ app.js
@@ -0,0 +1,28 @@
+//qustn 1
+var customerPrivateName="";
+
+//qustn 2
+//legal
+var noorAhmed="";  //camelcase
+var myvar="";
+var userAlert="";
+var customer_name=""; //underscore
+var $hopkeeper="";//$ sign
+
+//illegal
+
+// var 1noor=""; start from no
+// var 123=""; number
+// var customer number=""; space
+// var alert=""; js keyword
+// var "quality"=""; can't enclose quotation mark
+
+//qustn 3
+var qustn3_a="<h1>Rules for naming JS variables</h1> <br><br><br>";
+var qustn3_b="Variable names can only contain letters, numbers,dollar sign and underscore <br>";
+var qustn3_c="Variables must begin with a dollar, underscore or letters. For example $name, _name or name <br> ";
+var qustn3_d="Variable names are case sensitive <br>";
+var qustn3_e="Variable names should not be JS keywords <br>";
+
+var sum= qustn3_a+qustn3_b+qustn3_c+qustn3_d+qustn3_e;
+var print=document.write(sum);
